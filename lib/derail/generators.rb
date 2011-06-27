81a80fef4ca44e969250617709e52c8bce2882a0
Samuel Cochran <sj26@sj26.com>
2011-06-27 17:38:44 +0800

    Try to fix some generator issues.

--- lib/derail/generators.rb
+++ lib/derail/generators.rb
@@ -4,9 +4,35 @@ module Derail::Generators
 
   protected
 
+    def rvm *args
+      options = args.extract_options!
+      say_status :run, "rvm #{args * " "}" # unless options[:verbose] === false
+      system (["rvm"] + args) * " "
+    end
+
+    def rvm_run *args
+      args << args.extract_options!.merge(:verbose => false)
+      rvm "--with-rubies", "default-with-rvmrc", "exec", *args
+    end
+
     def bundle *args
-      say_status :run, "bundle #{args * ' '}"
-      system "bundle", *args
+      options = args.extract_options!
+      say_status :run, "bundle #{args * " "}" # unless options[:verbose] === false
+      rvm_run "bundle", *args
+    end
+
+    def bundle_run *args
+      args << args.extract_options!.merge(:verbose => false)
+      bundle "exec", *args
+    end
+
+    # Override run to run in our rvm/bundle
+    alias run bundle_run
+
+    # Override generate to run in bundle
+    def generate *args
+      say_status :generate, args * " "
+      bundle_run "rails", "generate", *args
     end
 
     def app_name
@@ -16,7 +42,5 @@ module Derail::Generators
     def app_slug
       app_name.underscore
     end
-
-    # TODO: Shared bits and pieces
   end
 end
