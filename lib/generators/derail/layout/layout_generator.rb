a029674b0c5c0c6ec46227931062d426c51407d4
Samuel Cochran <samuelc@lincintegrated.com>
2011-08-11 17:13:06 +0800

    Install HAML, too

--- lib/generators/derail/layout/layout_generator.rb
+++ lib/generators/derail/layout/layout_generator.rb
@@ -2,13 +2,19 @@ module Derail::Generators
   class LayoutGenerator < Base
     source_root File.expand_path("../../../../templates/layout", __FILE__)
 
+    def install_haml_rails
+      insert_into_file "Gemfile", "gem 'haml-rails'\n", :after => "# Views helpers\n"
+
+      bundle_install
+    end
+
     def remove_erb
-      remove_file "app/views/layout/application.erb.rb"
+      remove_file "app/views/layouts/application.erb.rb"
     end
 
     def copy_haml_layouts
-      copy_file "application.html.haml", "app/views/layout/application.html.haml"
-      copy_file "canvas.html.haml", "app/views/layout/canvas.html.haml"
+      copy_file "application.html.haml", "app/views/layouts/application.html.haml"
+      copy_file "canvas.html.haml", "app/views/layouts/canvas.html.haml"
     end
   end
 end
