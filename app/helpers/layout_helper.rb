44ff548e08ff4153ce3d272a7f9b9abb759bce2c
Samuel Cochran <samuelc@lincintegrated.com>
2011-08-12 09:30:26 +0800

    Better body application class generation

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,22 +1,28 @@
 module LayoutHelper
+  def application_name
+    Rails.application.class.name.split('::').first.underscore
+  end
+
   def modular_controller_name
     @modular_controller_name ||= controller.class.name.sub(/Controller$/, '').underscore.dasherize
   end
 
   def body_classes
     @body_classes ||= [
-      (@@body_class_app ||= Rails.application.class.name.parameterize),
+      application_name.dasherize,
+      # TODO: Add class for each module
       modular_controller_name.parameterize,
       [modular_controller_name, action_name].join('-').parameterize,
       ([modular_controller_name, action_name, params[:id]].join('-').parameterize if params[:id].present?),
       with_sidebar_class,
-    ].compact
+    ].flatten.compact
   end
 
   def title
     [area(:title).presence, "Career Finder"].compact.join ' - '
   end
 
+  # TODO: Expand into feature or area classes...
   def with_sidebar_class
     ('with-sidebar' if area(:sidebar).present?)
   end
